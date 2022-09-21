ba4ce8aad700790a9c8e26fbca2d00a6e6dbd328
Matvei Vitalie <vitaliematvei@gmail.com>
2022-09-21 12:52:39 +0300

    Created the project

---
+++ Transposition/Source/MainComponent.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class MainComponent  : public juce::Component
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
